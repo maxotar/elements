590542050d433dc5bd463522522c3a7b965a6849
“maxotar” <maxotar@users.noreply.github.com>
2018-06-17 18:59:32 -0700

    Add some light docstrings to the class.

--- elements.py
+++ elements.py
@@ -15,7 +15,14 @@ def gcc2lbin(g):
 
 
 class Elements:
+    '''
+    This class creates a periodic table object.
+    '''
+
     def __init__(self):
+        '''
+        Takes no parameters, simply loads the whole table into one object.
+        '''
         self._table = pd.read_csv('elements.csv', index_col='Element')
 
         self._table['Melting Point (C)'] = self._table.apply(
@@ -54,6 +61,10 @@ class Elements:
         self._table = self._table.rename_axis('Element')
 
     def matrix(self, elements):
+        '''
+        Converts the dataframe into a nested list
+        with the first row being the headers.
+        '''
         if not isinstance(elements, list):
             elements = [elements]
         mytable = self._table[elements]
@@ -63,6 +74,10 @@ class Elements:
         return matrix
 
     def markdown(self, elements):
+        '''
+        Creates a markdown formatted table of the elements that are
+        passed as arguments.
+        '''
         if not isinstance(elements, list):
             elements = [elements]
         data = self.matrix(elements)
@@ -81,21 +96,25 @@ class Elements:
         return markdownTable
 
     def markdownFullTable(self):
+        '''
+        Returns a fully formatted periodic table in markdown format
+        with the atomic number of each element.
+        '''
         return ('|T|H|E||P|E|R|I|O|D|I|C||T|A|B|L|E|'
-                    '|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|'
-                    '|||||||||||||||||||'
-                    '|||||||||||||||||||'
-                    '|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|'
-                    '|||||||||||||||||||'
-                    '|||||||||||||||||||'
-                    '|H^1|||||||||||||||||He^2|'
-                    '|Li^3|Be^4|||||||||||B^5|C^6|N^7|O^8|F^9|Ne^10|'
-                    '|Na^11|Mg^12|||||||||||Al^13|Si^14|P^15|S^16|Cl^17|Ar^18|'
-                    '|K^19|Ca^20|Sc^21|Ti^22|V^23|Cr^24|Mn^25|Fe^26|Co^27|Ni^28|Cu^29|Zn^30|Ga^31|Ge^32|As^33|Se^34|Br^35|Kr^36|'
-                    '|Rb^37|Sr^38|Y^39|Zr^40|Nb^41|Mo^42|Tc^43|Ru^44|Rh^45|Pd^46|Ag^47|Cd^48|In^49|Sn^50|Sb^51|Te^52|I^53|Xe^54|'
-                    '|Cs^55|Ba^56|La^57|Hf^72|Ta^73|W^74|Re^75|Os^76|Ir^77|Pt^78|Au^79|Hg^80|Ti^81|Pb^82|Bi^83|Po^84|At^85|Rn^86|'
-                    '|Fr^87|Ra^88|Ac^89|Rf^104|Db^105|Sg^106|Bh^107|Hs^108|Mt^109|Ds^110|Rg^111|Cn^112|Nh^113|Fl^114|Mc^115|Lv^116|Ts^117|Og^118|'
-                    '|||||||||||||||||||'
-                    '|||||||||||||||||||'
-                    '||||Ce^^58|Pr^59|Nd^60|Pm^61|Sm^62|Eu^63|Gd^64|Tb^65|Dy^66|Ho^67|Er^68|Tm^69|Yb^70|Lu^71|||'
-                    '||||Th^90|Pa^91|U^92|Np^93|Pu^94|Am^95|Cm^96|Bk^97|Cf^98|Es^99|Fm^100|Md^101|No^102|Lr^103|||')
+                '|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|:-|'
+                '|||||||||||||||||||'
+                '|||||||||||||||||||'
+                '|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|'
+                '|||||||||||||||||||'
+                '|||||||||||||||||||'
+                '|H^1|||||||||||||||||He^2|'
+                '|Li^3|Be^4|||||||||||B^5|C^6|N^7|O^8|F^9|Ne^10|'
+                '|Na^11|Mg^12|||||||||||Al^13|Si^14|P^15|S^16|Cl^17|Ar^18|'
+                '|K^19|Ca^20|Sc^21|Ti^22|V^23|Cr^24|Mn^25|Fe^26|Co^27|Ni^28|Cu^29|Zn^30|Ga^31|Ge^32|As^33|Se^34|Br^35|Kr^36|'
+                '|Rb^37|Sr^38|Y^39|Zr^40|Nb^41|Mo^42|Tc^43|Ru^44|Rh^45|Pd^46|Ag^47|Cd^48|In^49|Sn^50|Sb^51|Te^52|I^53|Xe^54|'
+                '|Cs^55|Ba^56|La^57|Hf^72|Ta^73|W^74|Re^75|Os^76|Ir^77|Pt^78|Au^79|Hg^80|Ti^81|Pb^82|Bi^83|Po^84|At^85|Rn^86|'
+                '|Fr^87|Ra^88|Ac^89|Rf^104|Db^105|Sg^106|Bh^107|Hs^108|Mt^109|Ds^110|Rg^111|Cn^112|Nh^113|Fl^114|Mc^115|Lv^116|Ts^117|Og^118|'
+                '|||||||||||||||||||'
+                '|||||||||||||||||||'
+                '||||Ce^^58|Pr^59|Nd^60|Pm^61|Sm^62|Eu^63|Gd^64|Tb^65|Dy^66|Ho^67|Er^68|Tm^69|Yb^70|Lu^71|||'
+                '||||Th^90|Pa^91|U^92|Np^93|Pu^94|Am^95|Cm^96|Bk^97|Cf^98|Es^99|Fm^100|Md^101|No^102|Lr^103|||')
